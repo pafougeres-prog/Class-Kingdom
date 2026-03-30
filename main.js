0387116ed2993bca1f26ce0be5bc0e5f71ec7639
pafougeres-prog <pa.fougeres@gmail.com>
2026-03-30 13:37:23 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,65 @@
+const supabaseUrl = "https://zfseazpheehiawebxrgx.supabase.co";
+const supabaseKey = "sb_publishable_2NpNBMV2yoVX5kNKLZyzPA_WCCEGt2Y";
+
+const { createClient } = supabase;
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+let playerId = null;
+
+async function login() {
+  const nickname = document.getElementById("nickname").value.trim().toLowerCase();
+  const password = document.getElementById("password").value.trim();
+
+  if (!nickname || !password) {
+    alert("Enter nickname and password");
+    return;
+  }
+
+  let { data, error } = await supabaseClient
+    .from("players")
+    .select("*")
+    .eq("nickname", nickname)
+    .eq("password", password);
+
+  if (error || data.length === 0) {
+    alert("Wrong nickname or password");
+    return;
+  }
+
+  playerId = data[0].id;
+  updateUI(data[0]);
+}
+
+function updateUI(player) {
+  document.getElementById("player").innerText = "Player: " + player.nickname;
+  document.getElementById("tribe").innerText = "Classroom: " + player.tribe;
+  document.getElementById("gold").innerText = "Gold: " + player.gold;
+  document.getElementById("player_class").src = player.player_class;
+
+  document.getElementById("stats").innerText =
+    "❤️ " + player.health +
+    " | 🛡️ " + player.armor +
+    " | ⚔️ " + player.attack;
+}
+
+async function addGold() {
+  if (!playerId) {
+    alert("Login first");
+    return;
+  }
+
+  let { data } = await supabaseClient
+    .from("players")
+    .select("*")
+    .eq("id", playerId)
+    .single();
+
+  let newGold = data.gold + 10;
+
+  await supabaseClient
+    .from("players")
+    .update({ gold: newGold })
+    .eq("id", playerId);
+
+  document.getElementById("gold").innerText = "Gold: " + newGold;
+}
