8c6848a8fc4d60107244f5f7f0da1744c3c7478b
pafougeres-prog <pa.fougeres@gmail.com>
2026-03-30 13:39:27 +0700

    Create quest1.js

---
+++ quest1.js
@@ -0,0 +1,41 @@
+function startQuest1() {
+  document.getElementById("quest").innerHTML = `
+    <p>🧙‍♂️ "Hello traveler! I love sticks! Do you know your family?"</p>
+    <button onclick="q1_ex1()">Start</button>
+  `;
+}
+
+function q1_ex1() {
+  document.getElementById("quest").innerHTML = `
+    <p>🧙‍♂️ "What is mother?"</p>
+
+    <button onclick="correctAnswer()">แม่</button>
+    <button onclick="wrongAnswer()">พ่อ</button>
+  `;
+}
+
+function correctAnswer() {
+  alert("Correct! +1 gold");
+  addGoldSimple(1);
+}
+
+function wrongAnswer() {
+  alert("Wrong!");
+}
+
+async function addGoldSimple(amount) {
+  let { data } = await supabaseClient
+    .from("players")
+    .select("*")
+    .eq("id", playerId)
+    .single();
+
+  let newGold = data.gold + amount;
+
+  await supabaseClient
+    .from("players")
+    .update({ gold: newGold })
+    .eq("id", playerId);
+
+  document.getElementById("gold").innerText = "Gold: " + newGold;
+}
