da171dee7a6c0f7aad3b089b510dad6b24f891f7
pafougeres-prog <pa.fougeres@gmail.com>
2026-04-03 22:27:46 +0700

    Update items.js

--- items.js
+++ items.js
@@ -1,17 +1,36 @@
-//LISTE DES ITEMS//
 const items = [
-  // COMMON (80%)
-  { name: "Old Stick", rarity: "common", attack: 1 },
-  { name: "Pointed stick", rarity: "common", attack: 2 },
+  // WEAPONS
+  { name: "Wood Stick", type: "weapon", rarity: "common", attack: 2 },
+  { name: "Iron Sword", type: "weapon", rarity: "rare", attack: 5 },
+  { name: "Knight Blade", type: "weapon", rarity: "epic", attack: 10 },
+  { name: "Dragon Slayer", type: "weapon", rarity: "legendary", attack: 20 },
 
-  // UNCOMMON (10%)
-  { name: "stick club", rarity: "uncommon", attack: 4 },
-
-  // RARE (5%)
-  { name: "spiked club", rarity: "rare", attack: 6 },
-
-  // LEGENDARY (1%)
-  { name: "nailed board", rarity: "legendary", attack: 10 }
+  // HELMETS
+  { name: "Cloth Hat", type: "helmet", rarity: "common", armor: 1 },
+  { name: "Iron Helmet", type: "helmet", rarity: "rare", armor: 3 },
+  { name: "Knight Helm", type: "helmet", rarity: "epic", armor: 6 },
+  { name: "Dragon Crown", type: "helmet", rarity: "legendary", armor: 10 },
+ // ARMORS
+  { name: "leather top", type: "helmet", rarity: "common", armor: 1 },
+  { name: "Iron cuirass", type: "helmet", rarity: "rare", armor: 3 },
+  { name: "Knight plates", type: "helmet", rarity: "epic", armor: 6 },
+  { name: "Dragon skin", type: "helmet", rarity: "legendary", armor: 10 },
+  // GLOVES
+  { name: "leather gloves", type: "helmet", rarity: "common", armor: 1 },
+  { name: "spiked mittens", type: "helmet", rarity: "rare", armor: 3 },
+  { name: "Knight plates", type: "helmet", rarity: "epic", armor: 6 },
+  { name: "Dragon fist", type: "helmet", rarity: "legendary", armor: 10 },
+ // BELTS
+  { name: "Quartz belt", type: "helmet", rarity: "common", attack: 2 },
+  { name: "Emerald belt", type: "helmet", rarity: "rare", attack: 4 },
+  { name: "Ruby belt", type: "helmet", rarity: "epic", attack: 6 },
+  { name: "Diamond belt", type: "helmet", rarity: "legendary", attack: 10 },
+ // BOOTS
+  { name: "leather boots", type: "helmet", rarity: "common", armor: 1 },
+  { name: "spiked boots", type: "helmet", rarity: "rare", armor: 3 },
+  { name: "Knight greaves", type: "helmet", rarity: "epic", armor: 6 },
+  { name: "Dragon fist", type: "helmet", rarity: "legendary", armor: 10 },
+  
 ];
 
 
